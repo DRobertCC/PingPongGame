4a63e3144247adeafaa8580c27e8b0f15f29db4a
Drobertcc <robert.dancso@gmail.com>
2019-11-18 09:39:36 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace PingPongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.difficultyMenu_Label = new System.Windows.Forms.Label();
+            this.topScore_Label = new System.Windows.Forms.Label();
+            this.pause_Label = new System.Windows.Forms.Label();
+            this.mainMenu_Label = new System.Windows.Forms.Label();
+            this.menu_Label = new System.Windows.Forms.Label();
+            this.difficultyLevel_Label = new System.Windows.Forms.Label();
+            this.score_Label = new System.Windows.Forms.Label();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new OvalPictureBox();
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.difficultyMenu_Label);
+            this.playground.Controls.Add(this.topScore_Label);
+            this.playground.Controls.Add(this.pause_Label);
+            this.playground.Controls.Add(this.mainMenu_Label);
+            this.playground.Controls.Add(this.menu_Label);
+            this.playground.Controls.Add(this.difficultyLevel_Label);
+            this.playground.Controls.Add(this.score_Label);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(1262, 673);
+            this.playground.TabIndex = 0;
+            // 
+            // difficultyMenu_Label
+            // 
+            this.difficultyMenu_Label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.difficultyMenu_Label.AutoSize = true;
+            this.difficultyMenu_Label.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficultyMenu_Label.Location = new System.Drawing.Point(415, 82);
+            this.difficultyMenu_Label.Name = "difficultyMenu_Label";
+            this.difficultyMenu_Label.Size = new System.Drawing.Size(419, 402);
+            this.difficultyMenu_Label.TabIndex = 7;
+            this.difficultyMenu_Label.Text = "Choose Difficulty\r\n\r\n1 - Easy\r\n2 - Medium\r\n3 - Hard\r\nESC - Quit Game";
+            this.difficultyMenu_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // topScore_Label
+            // 
+            this.topScore_Label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.topScore_Label.AutoSize = true;
+            this.topScore_Label.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topScore_Label.Location = new System.Drawing.Point(1192, 597);
+            this.topScore_Label.Name = "topScore_Label";
+            this.topScore_Label.Size = new System.Drawing.Size(58, 67);
+            this.topScore_Label.TabIndex = 4;
+            this.topScore_Label.Text = "0";
+            this.topScore_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pause_Label
+            // 
+            this.pause_Label.AutoSize = true;
+            this.pause_Label.BackColor = System.Drawing.Color.Transparent;
+            this.pause_Label.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pause_Label.Location = new System.Drawing.Point(113, 44);
+            this.pause_Label.Name = "pause_Label";
+            this.pause_Label.Size = new System.Drawing.Size(316, 67);
+            this.pause_Label.TabIndex = 6;
+            this.pause_Label.Text = "Game Paused";
+            this.pause_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainMenu_Label
+            // 
+            this.mainMenu_Label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.mainMenu_Label.AutoSize = true;
+            this.mainMenu_Label.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainMenu_Label.Location = new System.Drawing.Point(-12, 178);
+            this.mainMenu_Label.Name = "mainMenu_Label";
+            this.mainMenu_Label.Size = new System.Drawing.Size(501, 268);
+            this.mainMenu_Label.TabIndex = 5;
+            this.mainMenu_Label.Text = "Welcome to Pong\r\n\r\nF1 - Start New Game\r\nESC - Quit Game\r\n";
+            this.mainMenu_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menu_Label
+            // 
+            this.menu_Label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.menu_Label.AutoSize = true;
+            this.menu_Label.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menu_Label.Location = new System.Drawing.Point(777, 149);
+            this.menu_Label.Name = "menu_Label";
+            this.menu_Label.Size = new System.Drawing.Size(501, 268);
+            this.menu_Label.TabIndex = 5;
+            this.menu_Label.Text = "Game Over\r\n\r\nF1 - Start New Game\r\nESC - Quit Game\r\n";
+            this.menu_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // difficultyLevel_Label
+            // 
+            this.difficultyLevel_Label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.difficultyLevel_Label.AutoSize = true;
+            this.difficultyLevel_Label.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficultyLevel_Label.Location = new System.Drawing.Point(489, 597);
+            this.difficultyLevel_Label.Name = "difficultyLevel_Label";
+            this.difficultyLevel_Label.Size = new System.Drawing.Size(321, 67);
+            this.difficultyLevel_Label.TabIndex = 3;
+            this.difficultyLevel_Label.Text = "MediumGame";
+            this.difficultyLevel_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // score_Label
+            // 
+            this.score_Label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.score_Label.AutoSize = true;
+            this.score_Label.Font = new System.Drawing.Font("Comic Sans MS", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_Label.Location = new System.Drawing.Point(12, 597);
+            this.score_Label.Name = "score_Label";
+            this.score_Label.Size = new System.Drawing.Size(189, 67);
+            this.score_Label.TabIndex = 3;
+            this.score_Label.Text = "Score: ";
+            this.score_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Red;
+            this.racket.Location = new System.Drawing.Point(540, 536);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(200, 25);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Black;
+            this.ball.Location = new System.Drawing.Point(445, 531);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox racket;
+        private OvalPictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label score_Label;
+        private System.Windows.Forms.Label menu_Label;
+        private System.Windows.Forms.Label pause_Label;
+        private System.Windows.Forms.Label topScore_Label;
+        private System.Windows.Forms.Label difficultyMenu_Label;
+        private System.Windows.Forms.Label mainMenu_Label;
+        private System.Windows.Forms.Label difficultyLevel_Label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PingPongGame
+{
+    public partial class Form1 : Form
+    {
+        public int ballSpeedX; // horizontal movement direction & speed of the ball (-:left, +: right) 
+        public int ballSpeedY;  // vertical movement direction & speed of the ball (-: upward, +: down)
+        public int width;   // width of the playfield
+        public int height;     // height of the playfield
+
+        public int racketInitialWith;
+
+        public int fasterperNhit;
+        public float racketWidthShrinkMultiplier = 1; // default = 1
+
+        public Random r = new Random();
+
+        public int scores = 0;
+        public int topScore = 0;
+        public int topScoreE = 0;
+        public int topScoreM = 0;
+        public int topScoreH = 0;
+
+        int round;
+
+    // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+
+            Cursor.Hide();
+            //this.TopMost = true;    // Always on top
+            this.FormBorderStyle = FormBorderStyle.None;  // No Border & Header bar
+            this.Bounds = Screen.PrimaryScreen.Bounds;  // Fullscreen
+
+            width = playground.Width;
+            height = playground.Height;
+            Console.WriteLine(width + " : " + height);
+
+            racketInitialWith = (width / 4);
+
+            racket.Top = playground.Bottom - (playground.Bottom / 10);  // Racket always on the bottom of the screen
+
+            score_Label.Visible = false;
+            topScore_Label.Visible = false;
+            racket.Visible = false;
+            ball.Visible = false;
+
+            mainMenu_Label.Left = (width / 2) - (mainMenu_Label.Width / 2);
+            mainMenu_Label.Top = (height / 2) - (mainMenu_Label.Height / 2);
+            mainMenu_Label.Visible = true;
+
+            difficultyMenu_Label.Left = (width / 2) - (difficultyMenu_Label.Width / 2);
+            difficultyMenu_Label.Top = (height / 2) - (difficultyMenu_Label.Height / 2);
+            difficultyMenu_Label.Visible = false;
+
+            menu_Label.Left = (width / 2) - (menu_Label.Width / 2);
+            menu_Label.Top = (height / 2) - (menu_Label.Height / 2);
+            menu_Label.Visible = false;
+
+            pause_Label.Left = (width / 2) - (pause_Label.Width / 2);
+            pause_Label.Top = (height / 2) - (pause_Label.Height / 2);
+            pause_Label.Visible = false;
+
+            difficultyLevel_Label.Left = (width / 2) - (difficultyLevel_Label.Width / 2);
+            difficultyLevel_Label.Top = height - (height / 13);
+            difficultyLevel_Label.Visible = false;
+
+            score_Label.Left = width / 75;
+            score_Label.Top = height - (height / 13);
+            topScore_Label.Left = width - (width / 5);
+            topScore_Label.Top = height - (height / 13);
+
+            ballSpeedX = r.Next(-4, 4);    // horizontal movement direction of the ball
+            ballSpeedY = r.Next(3, 4);    // vertical movement direction of the ball
+
+            ball.Top = r.Next(50, height - (height / 2)); // Initial Y-position of the ball 
+            ball.Left = r.Next(50, width - 50); // Initial X-position of the ball
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X - (racket.Width / 2); // Set the center of the racket to the position of the cursor
+            ball.Left += ballSpeedX; //Move the ball
+            ball.Top += ballSpeedY;
+
+        // Racket collision
+            if (ball.Bottom >= racket.Top && /*ball.Bottom <= racket.Bottom && */ball.Left <= racket.Right && ball.Right >= racket.Left)
+            // orig:
+          //if (ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left && ball.Right <= racket.Right)
+            {
+                // Ball is getting faster after every N hits and the racket smaller if certain level choosen
+                if (++round % fasterperNhit == 0)
+                {
+                    ballSpeedY++;
+                    ballSpeedX++;
+                    racket.Width = (int)(racket.Width * racketWidthShrinkMultiplier);
+                }
+                ballSpeedY = -ballSpeedY; // Change direction
+                scores += 1;
+                score_Label.Text = "Score: " + scores.ToString();
+
+                playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255)); // Get a random dark RGB Color
+            }
+
+        // Direction changes when ball reaches playgrounds borders
+            if (ball.Left <= playground.Left)
+            {
+                ballSpeedX = -ballSpeedX;
+            }
+
+            if (ball.Right >= playground.Right)
+            {
+                ballSpeedX = -ballSpeedX;
+            }
+
+            if (ball.Top <= playground.Top)
+            {
+                ballSpeedY = -ballSpeedY;
+            }
+        // in case of Game over
+            if (ball.Bottom >= playground.Bottom) 
+            {
+                timer1.Enabled = false;
+                menu_Label.Visible = true;
+
+                switch (difficultyLevel_Label.Text)
+                {
+                    case "Easy Game":
+                        if (scores > topScoreE)
+                        {
+                            topScoreE = scores;
+                            topScore_Label.Text = "Top Score: " + topScoreE.ToString();
+                            //topScoreVar_Label.Text = (Convert.ToInt32(topScoreVar_Label.Text) < scores) ? scores.ToString() : topScoreVar_Label.Text;
+                            topScore_Label.Visible = true;
+                        }
+                        break;
+                    case "Medium Game":
+                        if (scores > topScoreM)
+                        {
+                            topScoreM = scores;
+                            topScore_Label.Text = "Top Score: " + topScoreM.ToString();
+                            topScore_Label.Visible = true;
+                        }
+                        break;
+                    case "Hard Game":
+                        if (scores > topScoreH)
+                        {
+                            topScoreH = scores;
+                            topScore_Label.Text = "Top Score: " + topScoreH.ToString();
+                            topScore_Label.Visible = true;
+                        }
+                        break;
+                }
+
+                //if (Convert.ToInt32(topScoreVar_Label.Text) < scores)
+                //{
+                //    topScoreVar_Label.Text = topScore.ToString();
+                //}
+                ////topScoreVar_Label.Text = (Convert.ToInt32(topScoreVar_Label.Text) < scores) ? scores.ToString() : topScoreVar_Label.Text;
+
+                //if (scores > 0)
+                //{
+                //    topScore_Label.Visible = true;
+                //    topScoreVar_Label.Visible = true;
+                //}
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {   
+        // ESC button pressed
+            if (e.KeyCode == Keys.Escape) 
+            {
+                Cursor.Show();
+                timer1.Enabled = false;
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to exit?", "What!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+                if (!menu_Label.Visible && !pause_Label.Visible && !difficultyMenu_Label.Visible && !mainMenu_Label.Visible)
+                {
+                    timer1.Enabled = true;
+                    Cursor.Hide();
+                }
+                Cursor.Hide();
+            }
+        // Pause game
+            if (e.KeyCode == Keys.Space && !menu_Label.Visible && !difficultyMenu_Label.Visible && !mainMenu_Label.Visible)
+            {
+                if (timer1.Enabled)
+                {
+                    timer1.Enabled = false;
+                    pause_Label.Visible = true;
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                    pause_Label.Visible = false;
+
+                }
+            }
+        // Start game
+            if (e.KeyCode == Keys.F1 && mainMenu_Label.Visible)
+            {
+                mainMenu_Label.Visible = false;
+                difficultyMenu_Label.Visible = true;
+
+            }
+        // Restart game
+            if (e.KeyCode == Keys.F1 && !pause_Label.Visible)
+            {
+                timer1.Enabled = false;
+                menu_Label.Visible = false;
+                difficultyMenu_Label.Visible = true;
+
+            }
+        // Easy game
+            if (e.KeyCode == Keys.D1 && !pause_Label.Visible && difficultyMenu_Label.Visible)
+            {
+                topScore = topScoreE;
+                ResetGame(racketInitialWith, 5, "Easy Game");
+                racketWidthShrinkMultiplier = 1;
+            }
+            // Medium game
+            if (e.KeyCode == Keys.D2 && !pause_Label.Visible && difficultyMenu_Label.Visible)
+            {
+                topScore = topScoreM;
+                ResetGame((racketInitialWith / 2), 5, "Medium Game");
+                racketWidthShrinkMultiplier = 1;
+            }
+            // Hard game
+            if (e.KeyCode == Keys.D3 && !pause_Label.Visible && difficultyMenu_Label.Visible)
+            {
+                topScore = topScoreH;
+                ResetGame(racketInitialWith, 1, "Hard Game");
+                racketWidthShrinkMultiplier = 0.9f;
+            }
+        }
+
+        private void ResetGame(int racketWidhtP, int fasterperNhitP, string difficulty)
+        {
+            Cursor.Hide();
+            ball.Top = r.Next(50, height - (height / 2)); // Y(Vertical)-position of the ball 
+            ball.Left = r.Next(50, width - 50); // X(Horizontal)-position of the ball
+            ballSpeedX = r.Next(-4, 4);   // horizontal movement direction & speed of the ball (-:left, +: right) 
+            ballSpeedY = r.Next(3, 5);  // vertical movement direction & speed of the ball (-: upward, +: down)
+            scores = 0;
+            round = 0;
+            racket.Visible = true;
+            ball.Visible = true;
+            score_Label.Visible = true;
+            racket.Width = racketWidhtP;
+            fasterperNhit = fasterperNhitP;
+            difficultyLevel_Label.Text = difficulty;
+            score_Label.Text = "Score: 0";
+            menu_Label.Visible = false;
+            difficultyMenu_Label.Visible = false;
+            difficultyLevel_Label.Visible = true;
+
+            if (topScore > 0)
+            {
+                topScore_Label.Text = "Top Score: " + topScore.ToString();
+                topScore_Label.Left = width - (width / 5);
+                topScore_Label.Visible = true;
+            }
+            else
+            {
+                topScore_Label.Visible = false;
+            }
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //racket.Top = playground.Bottom - (playground.Bottom / 10);  // Racket always on the bottom of the screen
+        }
+
+    }
+}
